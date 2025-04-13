def6c9ca31e7ba89805ab449372440da7e6b040a
Matias Laime <matulai1221@gmail.com>
2025-04-13 20:33:54 -0300

    Refactor: change to navigation by links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,35 @@
 "use client";
-import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 import Link from "next/link";
 
 const navItems = [
   { label: "SOBRE MÍ", href: "/" },
-  { label: "PROYECTOS", href: "proyectos" },
-  { label: "CONTACTO", href: "contacto" },
-  { label: "CONFIG", href: "config" },
+  { label: "PROYECTOS", href: "/proyectos" },
+  { label: "CONTACTO", href: "/contacto" },
+  { label: "CONFIG", href: "/config" },
 ];
 
 const Navbar = () => {
-  const [activeButton, setActiveButton] = useState("SOBRE MÍ");
+  const pathname = usePathname();
 
   return (
     <header className="h-32 w-full px-40 py-5 flex justify-between items-center border-b border-grey-light">
       {navItems.map(item => {
-        const isActive = item.label === activeButton;
+        const isActive = pathname == item.href;
 
         return (
           <Link
-            key={item.label}
+            key={item.href}
             href={item.href}
-            onClick={() => setActiveButton(item.label)}
             className={`px-4 py-3 w-80 flex items-center justify-center duration-100 transition-all ease-in-out
           ${
             isActive
               ? "bg-grey-dark text-black h-full"
-              : "bg-dark-light text-grey-light hover:bg-grey-dark hover:text-black"
+              : "bg-dark-light hover:bg-grey-dark hover:text-black"
           }`}
           >
-            <span className="text-4xl font-normal">{item.label}</span>
+            <span className="text-4xl">{item.label}</span>
           </Link>
         );
       })}
