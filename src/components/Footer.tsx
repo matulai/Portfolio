900eabc431fd3d345f759789d32422fad16873d0
Matias Laime <matulai1221@gmail.com>
2025-04-13 17:40:40 -0300

    Add: footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-start w-full p-4 border-t border-grey-light text-grey-light">
+      <span className="font-normal text-2xl">
+        © 2025 Matias Laime. All rights reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
