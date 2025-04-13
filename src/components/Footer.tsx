f63eebd7817a40eaf8c34c5328a98e7a009397df
Matias Laime <matulai1221@gmail.com>
2025-04-13 20:34:38 -0300

    Delete: delete class already added on body

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-start w-full p-4 border-t border-grey-light text-grey-light">
-      <span className="font-normal text-2xl">
+    <footer className="flex justify-self-start items-center w-full p-4 border-t border-grey-light">
+      <span className="text-2xl">
         © 2025 Matias Laime. All rights reserved.
       </span>
     </footer>
