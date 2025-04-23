3771374e329d0361cee9e93f8ec689bc7e282584
Matias Laime <matulai1221@gmail.com>
2025-04-23 01:09:00 -0300

    Refactor: change image type

--- src/types/proyect.tsx
+++ src/types/proyect.tsx
@@ -1,7 +1,10 @@
+import type { StaticImageData } from "next/image";
+
 interface Proyect {
   id: number;
+  title: string;
   description: string;
-  image: string;
+  image: StaticImageData;
   tags: string[];
 }
 
