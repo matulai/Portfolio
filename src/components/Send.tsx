8cd1a4a68e960adce13048316e176ca08223eba1
Matias Laime <matulai1221@gmail.com>
2025-04-23 23:11:41 -0300

    Add: send component

---
+++ src/components/Send.tsx
@@ -0,0 +1,19 @@
+const Send = () => {
+  return (
+    <svg
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 20.5V4.5L22 12.5L3 20.5ZM5 17.5L16.85 12.5L5 7.5V11L11 12.5L5 14V17.5ZM5 17.5V12.5V7.5V11V14V17.5Z"
+        fill="#D3D3D3"
+        fillOpacity="0.9"
+      />
+    </svg>
+  );
+};
+
+export default Send;
