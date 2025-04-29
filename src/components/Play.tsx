aed1ebf00fbbc789db55daa1d6b56f973f5b60c5
Matias Laime <matulai1221@gmail.com>
2025-04-29 01:42:51 -0300

    Add: Pley component

---
+++ src/components/Play.tsx
@@ -0,0 +1,20 @@
+const Play = () => {
+  return (
+    <svg
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-6 h-6 2xl:w-8 2xl:h-8"
+    >
+      <path
+        d="M6.6665 4L25.3332 16L6.6665 28V4Z"
+        fill="currentColor"
+        stroke="currentColor"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Play;
