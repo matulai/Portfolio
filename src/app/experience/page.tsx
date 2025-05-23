0731b73a6b0d2d83204b77a52779ffb95501e300
Matias Laime <matulai1221@gmail.com>
2025-05-23 20:57:45 -0300

    Add: experience map component

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,15 @@
+import ExperienceMap from "@/components/ExperienceMap";
+import AnimatedTitle from "@/components/AnimatedTitle";
+import PageInfo from "@/components/PageInfo";
+
+const Experience = () => {
+  return (
+    <>
+      <AnimatedTitle text="EXPERIENCIA" />
+      <ExperienceMap />
+      <PageInfo text="Aquí podes encontrar mis experiencias laborales" />
+    </>
+  );
+};
+
+export default Experience;
---
+++ src/components/ExperienceMap.tsx
@@ -0,0 +1,9 @@
+const ExperienceMap = () => {
+  return (
+    <div className="flex flex-col flex-1 align-center justify-center">
+      <span className=" text-2xl text-center">Coming Soon experience map</span>
+    </div>
+  );
+};
+
+export default ExperienceMap;
