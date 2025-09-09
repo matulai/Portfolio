d83ee7d28a2b9f6c77bef9e8a78ec9637056cb31
Matias Laime <matulai1221@gmail.com>
2025-09-09 17:08:02 -0300

    Add: new proyects

--- src/components/ProyectsInfo.tsx
+++ src/components/ProyectsInfo.tsx
@@ -6,16 +6,25 @@ import type Proyect from "@/types/proyect.ts";
 
 const proyects: Proyect[] = [
   {
-    id: 4,
+    id: 6,
+    title: "Star Wars Info",
+    description:
+      "Star Wars Info es una página web en la que podras buscar y ver toda la infomación acerca de la saga de Star Wars. Se uso la API SWAPI reborn para obtener los datos y para desarrollar la interfaz de usuario se utilizó Angular.",
+    image: "/images/star-wars-info.webp",
+    githubURL: "https://github.com/matulai/Star-Wars-Info",
+    liveURL: "https://precious-chaja-38d6a7.netlify.app/",
+  },
+  {
+    id: 7,
     title: "TikTok Clone",
     description:
-      "Tiktok Clone es una aplicación web en la cual su diseño se baso en la interfaz de TikTok, utiliza React para el desarrollo de interfaces, se desarrollo la app para mobiles utilizando React Native y la API fue desarrollada con Javalin.",
+      "Tiktok Clone es una página web en la cual su diseño se baso en la interfaz de TikTok, utiliza React para el desarrollo de interfaces, se desarrollo la app para móviles utilizando React Native y la API fue desarrollada con Javalin.",
     image: "/images/tiktok.webp",
     githubURL: "https://github.com/matulai/TikTok-model-TPs-UI",
     liveURL: "",
   },
   {
-    id: 5,
+    id: 8,
     title: "Rivals Tactics",
     description:
       "De la mano del motor de videojuegos Godot aparece Rivals Tactics que es un juego con el objetivo de desafíar a los jugadores a dominar el campo de batalla mediante estrategia pura y adaptabilidad en un combate 1 contra 1 de pura acción.",
@@ -24,10 +33,10 @@ const proyects: Proyect[] = [
     liveURL: "https://matulai.github.io/videojuego-rival-tactics/",
   },
   {
-    id: 6,
+    id: 9,
     title: "Eperdemic",
     description:
-      "Eperdemic es una aplicación desarrollada con Spring Boot que simula la propagación de un virus ficticio llamado Mechanus E534, en el cual se trabajo con diferentes tecnologias como JDBC, Hibernate, Spring, Neo4j, MongoDB, MySQL y Couchbase.",
+      "Eperdemic es una aplicación desarrollada con Spring Boot que simula la propagación de un virus ficticio llamado Mechanus E534, en el cual se trabajó con diferentes tecnologías como JDBC, Hibernate, Spring, Neo4j, MongoDB, MySQL y Couchbase.",
     image: "/images/eperdemic.webp",
     githubURL:
       "https://github.com/matulai/Estrategas-y-persistentes-TP/tree/main",
@@ -58,6 +67,24 @@ const proyects: Proyect[] = [
       "Portfolio es una aplicación web en la que me podras conocer, conocer acerca de mis proyectos personales, experiencias laborales y maneras de contactarme. Utiliza Next para construir la interfaz de usuario y TypeScript para tipar los datos.",
     image: "/images/portfolio.webp",
     githubURL: "https://github.com/matulai/Portfolio",
+    liveURL: "",
+  },
+  {
+    id: 4,
+    title: "Segui tus compras",
+    description:
+      "Segui tus compras es una aplicación en la cuál buscar y encontrar lo que quieras y comprarlo, dejar una reseña y guardarlo como favoritos, crea tu cuenta y comienza. Para el desarrollo se utilizo React y Typescript de Front y Spring Boot de Back.",
+    image: "/images/segui-tus-compras.webp",
+    githubURL: "https://github.com/matulai/PDES-2025C1-STC",
+    liveURL: "https://seguituscompras-frontend.onrender.com",
+  },
+  {
+    id: 5,
+    title: "Pronostico deportivo",
+    description:
+      "Si queres saber todo acerca de partidos de fútbol, equipos y jugadores entonces esta API te ayudara a obtener esos datos e incluso podras obtener predicciones de partidos y más!!. Para el desarrollo se utilizo Spring Boot y algunos LLM.",
+    image: "/images/pronostico-deportivo.webp",
+    githubURL: "https://github.com/matulai/unq-dapps-202501-GRUPOM",
     liveURL: "",
   },
 ];
